673dc8be02d503e6b257acb91cc66c81e16ebf38
StasShpigelman <60568641+StasShpigelman@users.noreply.github.com>
2022-03-01 15:14:09 +0200

    version

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ dev_requires = [
 
 setup(
     name="graphene-django",
-    version="3.0.0T",
+    version="3.0.99",
     description="Graphene Django integration",
     long_description=open("README.rst").read(),
     url="https://github.com/graphql-python/graphene-django",
